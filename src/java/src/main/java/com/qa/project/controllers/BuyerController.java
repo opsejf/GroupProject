07ed2385c766dcb5e8034107eb7eeffab039b7c5
JGA210602 <JGA210602>
2024-03-22 12:05:13 +0000

    Buyer domain

--- src/java/src/main/java/com/qa/project/controllers/BuyerController.java
+++ src/java/src/main/java/com/qa/project/controllers/BuyerController.java
@@ -1,12 +1,47 @@
 package com.qa.project.controllers;
 
+import com.qa.project.entities.Buyer;
 import com.qa.project.services.BuyerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
 public class BuyerController {
 
+    @Autowired
     private BuyerService service;
 
     public BuyerController(BuyerService service){
         this.service = service;
+    }
+
+    @GetMapping("/buyers")
+    public List<Buyer> getAll() {
+        return this.service.getAll();
+    }
+    @GetMapping("/buyer/{id}")
+    public Buyer get(@PathVariable int id) { return this.service.get(id); }
+
+    @PostMapping("/create-buyer")
+    public Buyer createBuyer(@RequestBody Buyer buyer) {
+        return this.service.createBuyer(buyer);
+    }
+
+    @PatchMapping("/update-buyer")
+    public Buyer updateBuyer(@PathVariable int id,
+                             @RequestParam(required = false) String firstName,
+                             @RequestParam(required = false) String lastName,
+                             @RequestParam(required = false) String email,
+                             @RequestParam(required = false) String mobileNo)
+    {
+        return this.service.updateBuyer(id, firstName, lastName, email, mobileNo);
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public Buyer removeBuyer(@PathVariable int id){
+        return this.service.removeBuyer(id);
     }
 }
--- src/java/src/main/java/com/qa/project/repos/BuyerRepo.java
+++ src/java/src/main/java/com/qa/project/repos/BuyerRepo.java
@@ -4,14 +4,11 @@ import com.qa.project.entities.Buyer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Repository
 public interface BuyerRepo extends JpaRepository<Buyer, Integer> {
 
-    @GetMapping("/getAll")
-    public List<Buyer> getAll() { return this.service.getAll(); }
-
-
-    @GetMapping("/get/{id}")
-    public Buyer get(@PathVariable int id) { };
 }
