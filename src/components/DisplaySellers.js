d5601d79dce050ffebce0d51374294dd124586e8
JGA210602 <JGA210602>
2024-03-25 12:05:52 +0000

    Updated front-end

--- src/components/DisplaySellers.js
+++ src/components/DisplaySellers.js
@@ -1,8 +1,9 @@
-import { React } from 'react';
+import { React, useState, useEffect } from 'react';
 import '../styles/displayUsers.css';
 import data from '../database/Properties.json';
 
 const DisplaySellers = (props) => {
+    const [userData, setUserData] = useState([]);
 
     // Checking which page we are on to display either buyer or seller
 
@@ -11,36 +12,54 @@ const DisplaySellers = (props) => {
     if (props.user == "Seller") {
         display = data.sellers;
     }
+
     else if (props.user == "Buyer") {
-        display = data.buyers;
+        useEffect(() => {
+               const fetchData = async () => {
+                   try {
+                       const response = await fetch('http://localhost:8080/buyers');
+                       if (!response.ok) {
+                           throw new Error('Failed to fetch data');
+                       }
+                       const jsonData = await response.json();
+                       setBuyersData(jsonData);
+                   } catch (error) {
+                       console.error('Error fetching data:', error);
+                   }
+               };
+
+               fetchData();
+        }, []);
     }
+
 
     return (
         <div className="total">
             
             <h1>Find A {props.user}</h1>
             <br />
+
             <div className = "scroll">
                 <table  name="sellerData">
-                    <tr className='headingHover'>
-                        {/* <th>{props.user}: </th> */}
-                        <th>Forename </th>
-                        <th>Surname </th>
-                        <th className='smallScreen' >Email </th>
-                    </tr>
-
-                    
-                    {display.map((details) => {
-                        return (
-                            <tr key={details.id} >
-                                {/* <td>{details.ID}</td> */}
-                                <td>{details.firstName}</td>
-                                <td>{details.lastName}</td>
-                                <td className='smallScreen'>{details.email}</td>
-                            </tr>
-                        )
-                    }
-                    )}
+                    <thead>
+                        <tr className='headingHover'>
+                            {/* <th>{props.user}: </th> */}
+                            <th>Forename </th>
+                            <th>Surname </th>
+                            <th className='smallScreen' >Email </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {userData.map((details) => {
+                            return (
+                                <tr key={details.id}>
+                                    <td>{details.first_name}</td>
+                                    <td>{details.last_name}</td>
+                                    <td className='smallScreen'>{details.email}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
                 </table>
             </div>
             
